8f50230b826ca8c52a660258d5dadddfdcd15c19
James Barrett <jhbarrett@hotmail.com>
2020-06-09 07:31:21 -0600

    Removed weather forecast functionality as well as counter and fetchdata pages.  Updated NavMenu to use code behind.

---
+++ BugTrackerBlazor/BugTrackerBlazor/Shared/NavMenuBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BugTrackerBlazor.Shared
+{
+    public class NavMenuBase : ComponentBase
+    {
+        public bool collapseNavMenu = true;
+
+        public string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        public void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
